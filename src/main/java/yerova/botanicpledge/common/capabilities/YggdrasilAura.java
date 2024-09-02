c60f4a0cca57dbc2e53f5450af272759e8f19918
Yerova <Yerova.Kalvin@gmail.com>
2024-09-02 22:13:42 +0200

    - improved Value Generation of YggdrasilAura to also include the biome you are in

--- src/main/java/yerova/botanicpledge/common/capabilities/YggdrasilAura.java
+++ src/main/java/yerova/botanicpledge/common/capabilities/YggdrasilAura.java
@@ -1,7 +1,9 @@
 package yerova.botanicpledge.common.capabilities;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.chunk.LevelChunk;
 import yerova.botanicpledge.common.utils.SimplePerlinNoise;
 import yerova.botanicpledge.setup.BotanicPledge;
@@ -9,6 +11,7 @@ import yerova.botanicpledge.setup.BotanicPledge;
 public class YggdrasilAura {
     private int genPerInstance;
     private final LevelChunk chunk;
+
     public YggdrasilAura(LevelChunk chunk) {
         this.genPerInstance = prepareAuraValue(chunk);
         this.chunk = chunk;
@@ -16,15 +19,42 @@ public class YggdrasilAura {
 
     public static int prepareAuraValue(LevelChunk chunk) {
         if (chunk.getLevel().isClientSide) return 0;
-        if (chunk.getLevel() instanceof ServerLevel) {
+        if (chunk.getLevel() instanceof ServerLevel serverLevel) {
+            double noiseValue = SimplePerlinNoise.noise(chunk.getPos().x * 0.1, chunk.getPos().z * 0.1);
+            double normalizedNoiseValue = (noiseValue + 1.0) / 2.0;
 
-            double noiseValue = SimplePerlinNoise.noise(chunk.getPos().x*0.1,chunk.getPos().z*0.1);
+            BlockPos chunkPos = chunk.getPos().getWorldPosition();
+            Biome biome = serverLevel.getBiome(chunkPos).value();
+            Biome.Precipitation precipitation = biome.getPrecipitationAt(chunkPos);
 
-            double normalizedValue = (noiseValue + 1.0) / 2.0;
+            float greennessFactor = calculateGreenness(biome, precipitation, chunkPos.getY());
 
-            return (int)(normalizedValue * 20);
+            return (int) (normalizedNoiseValue * 20 * greennessFactor);
         }
         return 1;
+    }
+
+    private static float calculateGreenness(Biome biome, Biome.Precipitation precipitation, int altitude) {
+        float greenness = 1.0f;
+
+        float temperature = biome.getBaseTemperature();
+        float rainfall = biome.getModifiedClimateSettings().downfall();
+
+        greenness *= Math.max(0, Math.min(temperature, 1.0f));
+        greenness *= Math.max(0, Math.min(rainfall, 1.0f));
+
+        if (precipitation == Biome.Precipitation.RAIN) {
+            greenness *= 1.2f;
+        } else if (precipitation == Biome.Precipitation.SNOW) {
+            greenness *= 0.8f;
+        } else {
+            greenness *= 0.9f;
+        }
+
+        float altitudeFactor = 1.0f - Math.min(altitude / 256.0f, 0.5f);
+        greenness *= altitudeFactor;
+
+        return Math.max(0.5f, Math.min(greenness, 2.0f));
     }
 
     public LevelChunk getChunk() {
@@ -35,7 +65,6 @@ public class YggdrasilAura {
         return genPerInstance;
     }
 
-
     public void saveNBTData(CompoundTag nbt) {
         nbt.putInt(BotanicPledge.MOD_ID + ".genPerInstance", genPerInstance);
     }
@@ -43,5 +72,4 @@ public class YggdrasilAura {
     public void loadNBTData(CompoundTag nbt) {
         genPerInstance = nbt.getInt(BotanicPledge.MOD_ID + ".genPerInstance");
     }
-
 }
